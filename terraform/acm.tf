2b6ca40d4a3ae8498ee33eee0753d8696709d38d
Timothy Coleman <timhc22@gmail.com>
2023-01-11 09:09:03 +0000

    Update certificate stuff

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,12 +1,14 @@
-resource "aws_acm_certificate" "cert" {
-  domain_name       =  var.WEBSITE_URL
-  validation_method = "DNS"
-
-  tags = {
-    Environment = "production"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#resource "aws_acm_certificate" "cert" {
+#  domain_name       =  var.WEBSITE_URL
+#  validation_method = "DNS"
+##  status = "ISSUED"
+##  provider = aws.virginia
+#
+#  tags = {
+#    Environment = "production"
+#  }
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -38,7 +38,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -107,10 +107,13 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
-#    cloudfront_default_certificate = true # todo is this needed?
+#    acm_certificate_arn = data.aws_acm_certificate.issued.id
+    acm_certificate_arn = var.certificate_arn
+#    acm_certificate_arn = aws_acm_certificate.cert.id
+    cloudfront_default_certificate = false # todo is this needed?
     # todo which of these?
     ssl_support_method = "sni-only"
 #    ssl_support_method = "vip"
+    minimum_protocol_version = "TLSv1"
   }
 }
