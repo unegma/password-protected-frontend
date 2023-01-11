2f5de4ace58bde2cdf68de52788d7a0561ed4454
Timothy Coleman <timhc22@gmail.com>
2023-01-11 05:19:26 +0000

    Add locals

---
+++ terraform/locals.tf
@@ -0,0 +1,12 @@
+# Credit to: "https://github.com/eunchurn/terraform-typescript-lambda-apigateway"
+# Credit to: "https://github.com/anwarkh"
+
+locals {
+  author = "Tim Coleman"
+  tags = {
+    Name = "Lambda"
+    GitRepo = "https://github.com/unegma"
+    ManagedBy = "Terraform"
+    Owner = "tim@unegma.com"
+  }
+}
