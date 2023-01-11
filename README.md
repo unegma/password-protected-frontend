8a6c44a8c96467c670f903b99860910a5773de04
Timothy Coleman <timhc22@gmail.com>
2023-01-11 10:54:24 +0000

    Add config for bucket

---
+++ README.md
@@ -0,0 +1,1 @@
+For this one, run terraform first, and then upload manually to the bucket from the s3 directory
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,13 +4,21 @@ resource "aws_s3_bucket" "bucket-1" {
   bucket = var.WEBSITE_URL
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket-1.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 data "aws_s3_bucket" "selected-bucket" {
   bucket = aws_s3_bucket.bucket-1.bucket
 }
 
 resource "aws_s3_bucket_website_configuration" "password-protected-lambda" {
   bucket = data.aws_s3_bucket.selected-bucket.bucket
-  #acl    = "public-read"
 
   index_document {
     suffix = "index.html"
