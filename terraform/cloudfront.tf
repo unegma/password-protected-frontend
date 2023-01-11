b20e40186bfe3e721cbaa5c1d8fb6001df518964
Timothy Coleman <timhc22@gmail.com>
2023-01-11 05:28:27 +0000

    Tweak cloudfront

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -7,8 +7,8 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    domain_name              = aws_s3_bucket.bucket-1.bucket_regional_domain_name
+    #origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
 
@@ -17,16 +17,16 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   comment             = "Some comment"
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
-  }
+#  logging_config {
+#    include_cookies = false
+#    bucket          = "mylogs.s3.amazonaws.com"
+#    prefix          = "myprefix"
+#  }
 
-  aliases = ["mysite.example.com", "yoursite.example.com"]
+  aliases = [var.WEBSITE_URL]
 
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
@@ -45,54 +45,54 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     function_association {
       event_type   = "viewer-request"
-      function_arn = aws_cloudfront_function.example.arn
+      function_arn = aws_lambda_function.lambda_edge_function.arn
     }
   }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
+#
+#  # Cache behavior with precedence 0
+#  ordered_cache_behavior {
+#    path_pattern     = "/content/immutable/*"
+#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#    target_origin_id = local.s3_origin_id
+#
+#    forwarded_values {
+#      query_string = false
+#      headers      = ["Origin"]
+#
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    min_ttl                = 0
+#    default_ttl            = 86400
+#    max_ttl                = 31536000
+#    compress               = true
+#    viewer_protocol_policy = "redirect-to-https"
+#  }
+#
+#  # Cache behavior with precedence 1
+#  ordered_cache_behavior {
+#    path_pattern     = "/content/*"
+#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id = local.s3_origin_id
+#
+#    forwarded_values {
+#      query_string = false
+#
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    min_ttl                = 0
+#    default_ttl            = 3600
+#    max_ttl                = 86400
+#    compress               = true
+#    viewer_protocol_policy = "redirect-to-https"
+#  }
 
   price_class = "PriceClass_200"
 
